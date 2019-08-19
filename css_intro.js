a2e960cddf8343ed26ff1718fd62b0b97fc773cd
DouglasMacKrell <douglasmackrell@pursuit.org>
2019-08-19 14:17:23 -0400

    toggle installed, not working. ignore the play-sound files

--- css_intro.js
+++ css_intro.js
@@ -1,3 +1,29 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var checkbox = document.querySelector('input[type="checkbox"]');
+  
+    function changeCSS(cssFile, cssLinkIndex) {
+
+        var oldlink = document.getElementsByTagName("link").item(cssLinkIndex);
+    
+        var newlink = document.createElement("link");
+        newlink.setAttribute("rel", "stylesheet");
+        newlink.setAttribute("type", "text/css");
+        newlink.setAttribute("href", cssFile);
+    
+        document.getElementsByTagName("head").item(0).replaceChild(newlink, oldlink);
+    }
+
+    checkbox.addEventListener('change', function () {
+      if (checkbox.checked) {
+        // do this
+        console.log('Checked');
+      } else {
+        // do that
+        console.log('Not checked');
+      }
+    });
+  });
+
 function changeCSS(cssFile, cssLinkIndex) {
 
     var oldlink = document.getElementsByTagName("link").item(cssLinkIndex);
