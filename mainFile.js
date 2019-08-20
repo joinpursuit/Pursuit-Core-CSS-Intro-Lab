01ba28e0dacb2039791c86c077136b059b8724ea
serami12 <stephanieramirez1@pursuit.org>
2019-08-20 10:21:37 -0400

    answers to lab

---
+++ mainFile.js
@@ -0,0 +1,10 @@
+//  const createNewCss = (questionOne, labLastQuestion) => {
+//      let oldLink = document.getElementById("oldLink");
+
+//      let newLink = document.createElement("link");
+//      newLink.setAttribute("rel", "stylesheet");
+//      newLink.setAttribute("type", "text/css");
+//      newLink.setAttribute("href", "labLastQuestion.css");
+//      document.getElementsByTagName("head").item(0).replaceChild(newLink, oldLink);
+
+//  }
