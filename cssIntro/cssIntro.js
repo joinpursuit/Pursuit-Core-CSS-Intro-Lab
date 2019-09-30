2c271960ca9c82428ab2ac5fd8effa321684287d
tawnyblvd <margara.sees@gmail.com>
2019-09-30 16:55:05 -0400

    update for changes while working on reset style button

--- cssIntro/cssIntro.js
+++ cssIntro/cssIntro.js
@@ -3,5 +3,18 @@ document.addEventListener("onclick", () => {
 })
 
 function switchStyle() {
-  
+
+  let currentStyle = document.getElementById("#originalStyle");
+  let alternateStyle = document.getElementById("#alternateStyle");
+
+  if (currentStyle) {
+
+    // document.getElementById("#originalStyle").href="cssIntro_blank.css";
+    currentStyle.setAttribute("href", "cssIntro_blank.css");
+
+  } else if (!currentStyle) {
+
+    // document.getElementById("#alternateStyle").href="cssIntro.css";
+    alternateStyle.setAttribute("href", "cssIntro.css");
+  }
 }
