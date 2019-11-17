8f7d56c95f19bbf697b087ff96c6020d39d72c8e
Jovanni <jovanniluna@pursuit>
2019-11-17 16:01:09 -0500

    Completeing problem 17

--- button.js
+++ button.js
@@ -1,7 +1,20 @@
-document.addEventListener("DOMContentLoaded" , () =>{
-    let submit = document.querySelector("#button")
-    submit.addEventListener("click", (element)=>{
-        element.preventDefault();
-        
+// document.getElementsByTagName('head')[0].getElementById('stylesheet').href='./website.css';
+
+document.addEventListener("DOCContentLoaded", (e)=>{
+    e.disableDefault();
+    const enableFont = () =>{
+        let style = document.querySelector("#stylesheet");
+        if(style.disabled){
+            style.disbled = false;
+        } else {
+            style.disabled = true
+        }
+    }
+        let submit = document.querySelector("#button")
+        submit.document.addEventListener("click", () =>{
+            enableFont()
     })
 })
+
+
+    // <a onclick="setStyleSheet('empty.css')" href="empty.css">Style 2</a>
