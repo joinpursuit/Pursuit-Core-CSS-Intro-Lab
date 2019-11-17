09eb9be035cf50b9e53512189b0058934dc3ded3
Danielle R Cherry <daniellecherry@pursuit.org>
2019-11-17 14:20:11 -0500

    FIrst commit

---
+++ index.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let cssbutton = document.querySelector("#cssbutton")
+    debugger
+
+        
+    cssbutton.addEventListener("click", (e) =>{
+
+        let oldLink = document.getElementsByTagName("link").item(cssLinkIndex)
+
+        let newLink = document.createElement("link")
+
+        debugger
+        console.log("hi")
+
+
+    })
+})
