72d5b41336ba27dd349abcc40b5f3a416c225666
Marvin Thompson <marvinthompson@pursuit.org>
2019-11-17 12:53:27 -0500

    lab complete

---
+++ lab.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let button = document.querySelector("#reset")
+    button.addEventListener("click", () => {
+        let main = document.querySelector("link")
+        let alt = document.createElement("link")
+        alt.setAttribute("rel", "stylesheet")
+        alt.setAttribute("type", "text/css")
+        alt.setAttribute("href", "./alt.css")
+        let parent = main.parentNode
+        parent.replaceChild(alt,main)
+
+    })   
+})
