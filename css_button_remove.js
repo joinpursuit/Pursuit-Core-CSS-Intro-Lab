2a145d8c4c769f89f31195f2b3afdeca10a7f3fb
John Jones Jr <jayjohnjones@gmail.com>
2019-11-17 14:26:17 -0500

    Lab complete

--- css_button_remove.js
+++ css_button_remove.js
@@ -1,3 +1,12 @@
 const changeCss = () =>{
-    
+        var oldlink = document.querySelector("link")
+        if(oldlink.id === "ogLink"){
+            oldlink.href = "./css_button_remove.css"
+            oldlink.id = "newLink"
+        } else {
+            oldlink.href = "./css_intro.css"
+            oldlink.id = "ogLink"
+        }
+        
+
 }
