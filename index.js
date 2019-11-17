3670bffdb0cdeaadaf64653296e75f3a2d91942b
patrickgeorgesmith <patricksmith@pursuit.org>
2019-11-17 12:19:55 -0500

    CssFinal Copy

--- index.js
+++
@@ -1,8 +0,0 @@
- console.log("hello");
- 
- let css = document.querySelector('#stylesheet').href
-            const nostyle =()=>{
-                document.querySelector('#stylesheet').href === css ?
-                    document.querySelector('#stylesheet').href = '' :
-                    document.querySelector('#stylesheet').href = css;
-            }
