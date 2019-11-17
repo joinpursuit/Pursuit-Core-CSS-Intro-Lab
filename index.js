d8e0b37a99e867a3fdd82020f5498d7ae14469dc
Kevin Wong <kevinwong@pursuit.org>
2019-11-17 15:18:46 -0500

    Question 17 completed

--- index.js
+++ index.js
@@ -3,18 +3,14 @@ document.addEventListener("DOMContentLoaded", () => {
     button.id="clearBtn";
     button.innerText = "Clear CSS";
     document.body.appendChild(button);
-    let link = document.querySelector("link");
-
-    if(reference === "./master.css"){
-        button.addEventListener("click", () => {
-            link.href = "./clear.css";
-        })
-    }
-    else {
-        button.addEventListener("click", () => {
+    button.addEventListener("click", () => {
+        let link = document.querySelector("#cssLink");
+        if(link.href === "file:///Users/kevinwong/Desktop/Pursuit/Assignments/Unit%202%20-%20Labs/Pursuit-Core-CSS-Intro-Lab/master.css"){
+            link.href = "./clear.css"
+        } else {
             link.href = "./master.css";
-        })
-    }
+        }
+    })
 })
 
 // const clickEvent = () => {
